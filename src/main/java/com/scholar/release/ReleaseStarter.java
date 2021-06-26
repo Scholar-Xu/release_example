628512741297e73a07441c94a8af30ca9e9cbe0d
scholar.xu <xvyingcai@163.com>
2021-06-26 15:11:04 +0800

    feat(release) add starter and helloController

---
+++ src/main/java/com/scholar/release/ReleaseStarter.java
@@ -0,0 +1,12 @@
+package com.scholar.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseStarter.class, args);
+    }
+}
---
+++ src/main/java/com/scholar/release/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.scholar.release.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping
+    public String getName(){
+        return "release-example say hello";
+    }
+}
